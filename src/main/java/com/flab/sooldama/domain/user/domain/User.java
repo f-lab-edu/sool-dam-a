aa3d9b9cd551525516c47391284b4b07a9060e95
dev-seokho <loyu78@gmail.com>
2022-10-06 16:06:52 +0900

    Comment: 새로운 어노테이션인 Mapper, Getter, Builder, AllArgsConstructor 에 대한 주석 추가

--- src/main/java/com/flab/sooldama/domain/user/domain/User.java
+++ src/main/java/com/flab/sooldama/domain/user/domain/User.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
-@Builder
-@AllArgsConstructor
+@Getter // 객체의 필드에 대한 기본 getter 를 생성해주는 lombok 의 어노테이션 입니다.
+@Builder // 객체에 빌더패턴을 사용할 수 있게 도와주는 lombok 의 어노테이션 입니다.
+@AllArgsConstructor // 해당 객체 내에 있는 모든 변수를 인수로 받는 생성자를 만들어주는 lombok 의 어노테이션 입니다.
 public class User {
 
     private Long id;
