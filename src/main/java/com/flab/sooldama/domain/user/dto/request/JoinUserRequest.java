2e0cea5ba8b0253384fe0afa0fbbe39e70d061f3
dev-seokho <loyu78@gmail.com>
2022-10-06 16:21:35 +0900

    Feat: DTO 폴더 구조 생성

---
+++ src/main/java/com/flab/sooldama/domain/user/dto/request/JoinUserRequest.java
@@ -0,0 +1,5 @@
+package com.flab.sooldama.domain.user.dto.request;
+
+public class JoinUserRequest {
+    // ToDo: 회원 가입 요청 로직 입력
+}
---
+++ src/main/java/com/flab/sooldama/domain/user/dto/response/JoinUserResponse.java
@@ -0,0 +1,5 @@
+package com.flab.sooldama.domain.user.dto.response;
+
+public class JoinUserResponse {
+    //Todo : 회원 가입 응답 로직 입력
+}
