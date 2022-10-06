466b24ef7492cb6bdfaac6688094d91d0ca2eea4
dev-seokho <loyu78@gmail.com>
2022-10-06 16:02:04 +0900

    Feat: User 객체에 Builder 패턴 구성

--- src/main/java/com/flab/sooldama/domain/user/domain/User.java
+++ src/main/java/com/flab/sooldama/domain/user/domain/User.java
@@ -1,9 +1,11 @@
 package com.flab.sooldama.domain.user.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class User {
 
