2b563135b524d4d928bc98c9596d5edc154ef805
sehun-kim <rogarithm@gmail.com>
2022-10-19 16:22:31 +0900

    Comment: BDD 형식에 맞게 테스트에 given, when, then 주석 추가

--- src/test/java/com/flab/sooldama/domain/user/service/UserServiceTest.java
+++ src/test/java/com/flab/sooldama/domain/user/service/UserServiceTest.java
@@ -18,7 +18,7 @@ class UserServiceTest {
     @Test
     @DisplayName("사용자가 회원가입하면 DB에 회원정보가 추가되나")
     public void userInfoAddedOnDB() {
-        // 회원가입할 사용자 객체 생성
+        //given 회원가입할 사용자 객체 생성
         JoinUserRequest request1 = JoinUserRequest.builder()
                 .id(1L)
                 .email("sehoon@fmail.com")
@@ -30,10 +30,10 @@ class UserServiceTest {
                 .createdAt("2022-10-07")
                 .build();
 
-        // service에서 dao에 회원가입 요청
+        //when service에서 dao에 회원가입 요청
         userService.insertUser(request1);
 
-        // DB에 가입한 회원 정보가 있는지 확인
+        //then DB에 가입한 회원 정보가 있는지 확인
         User joinedUser = userService.findUserById(request1.getId()).getUser();
         Assertions.assertThat(joinedUser.getId()).isEqualTo(1L);
     }
@@ -41,13 +41,13 @@ class UserServiceTest {
     @Test
     @DisplayName("없는 아이디로 사용자를 조회")
     public void findUserWithNonExistingId() {
-       // 없는 아이디를 만든다
+       //given 없는 아이디를 만든다
        Long wrongId = -1L;
 
-       // service에 없는 아이디에 대한 회원 정보를 요청
+       //when service에 없는 아이디에 대한 회원 정보를 요청
         JoinUserResponse wrongResponse = userService.findUserById(wrongId);
 
-        // 오류 발생
+        //then 오류 발생
         Assertions.assertThat(wrongResponse.getUser()).isNull();
     }
 }
