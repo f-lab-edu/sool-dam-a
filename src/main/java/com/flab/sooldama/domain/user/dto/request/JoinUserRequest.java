a8df91914568de6da7e8f03141b8959239e25848
sehun-kim <rogarithm@gmail.com>
2022-10-21 21:45:07 +0900

    Feat: request dto에 이메일 유효성 검증 어노테이션 추가

--- src/main/java/com/flab/sooldama/domain/user/dto/request/JoinUserRequest.java
+++ src/main/java/com/flab/sooldama/domain/user/dto/request/JoinUserRequest.java
@@ -1,6 +1,7 @@
 package com.flab.sooldama.domain.user.dto.request;
 
 import com.flab.sooldama.domain.user.domain.User;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 /*
  * @NoArgsConstructor 어노테이션은 파라미터가 없는 생성자를 만들어줍니다.
  * @AllArgsConstructor 어노테이션은 이 어노테이션이 붙은 클래스 안 모든 필드를 파라미터로 하는 생성자를 만들어줍니다.
+ * @Email 어노테이션은 해당 필드의 값이 유효한 이메일 주소인지를 검증합니다. 필드값이 null일 때도 유효하다고 판단해 true를 반환합니다.
  */
 @Getter
 @Builder
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class JoinUserRequest {
 
     @NotNull
+    @Email(message = "이메일 형식에 맞지 않습니다")
     private String email;
     @NotNull
     private String password;
