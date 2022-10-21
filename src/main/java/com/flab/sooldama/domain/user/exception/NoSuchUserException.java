cdb808d789d472ee2b279ee9ccf32ab55dffe581
sehun-kim <rogarithm@gmail.com>
2022-10-21 21:47:25 +0900

    Feat: 이메일 중복, 아이디에 해당하는 사용자가 없는 경우 예외 추가

---
+++ src/main/java/com/flab/sooldama/domain/user/exception/NoSuchUserException.java
@@ -0,0 +1,8 @@
+package com.flab.sooldama.domain.user.exception;
+
+public class NoSuchUserException extends RuntimeException {
+
+    public NoSuchUserException(String message) {
+        super(message);
+    }
+}
