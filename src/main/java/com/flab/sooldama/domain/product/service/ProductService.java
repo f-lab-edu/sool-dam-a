9dc3c273d3eb9ec94e19eafcce84760663055afc
dev-seokho <loyu78@gmail.com>
2022-10-23 02:32:42 +0900

    Feat: Mybatis 동적 쿼리를 사용해서 제품 보기 API 수정

--- src/main/java/com/flab/sooldama/domain/product/service/ProductService.java
+++ src/main/java/com/flab/sooldama/domain/product/service/ProductService.java
@@ -13,30 +13,8 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductMapper productMapper;
 
-    public List<ProductResponse> getProducts(Integer offset, Integer limit) {
-        List<Product> products = productMapper.selectProducts(offset, limit);
-        List<ProductResponse> productResponses = new ArrayList<>();
-
-        for (Product product : products) {
-            productResponses.add(
-                    ProductResponse.builder()
-                            .id(product.getId())
-                            .productCategoryId(product.getProductCategoryId())
-                            .name(product.getName())
-                            .price(product.getPrice())
-                            .imageUrl(product.getImageUrl())
-                            .description(product.getDescription())
-                            .abv(product.getAbv())
-                            .capacity(product.getCapacity())
-                            .build());
-        }
-
-        return productResponses;
-    }
-
-    public List<ProductResponse> getProductsByCategoryId(
-            Integer offset, Integer limit, Long categoryId) {
-        List<Product> products = productMapper.selectProductsByCategoryId(offset, limit, categoryId);
+    public List<ProductResponse> getProducts(Integer offset, Integer limit, Long categoryId) {
+        List<Product> products = productMapper.selectProducts(offset, limit, categoryId);
         List<ProductResponse> productResponses = new ArrayList<>();
 
         for (Product product : products) {
