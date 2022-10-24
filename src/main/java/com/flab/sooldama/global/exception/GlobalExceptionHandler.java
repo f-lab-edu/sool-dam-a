e0554c1948f45458589d40c07487ce6ccbe81732
sehun-kim <rogarithm@gmail.com>
2022-10-24 22:23:13 +0900

    Feat: 회원가입에서 발생할 수 있는 예외 처리 클래스 추가

--- src/main/java/com/flab/sooldama/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/flab/sooldama/global/exception/GlobalExceptionHandler.java
@@ -1,2 +1,24 @@
-package com.flab.sooldama.global.exception;public class GlobalExceptionHandler {
+package com.flab.sooldama.global.exception;
+
+import com.flab.sooldama.domain.user.exception.DuplicateEmailExistsException;
+import com.flab.sooldama.domain.user.exception.NoSuchUserException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateEmailExistsException.class)
+    public ResponseEntity<HttpStatus> handleDuplicateEmailExistsException(
+            DuplicateEmailExistsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(NoSuchUserException.class)
+    public ResponseEntity<HttpStatus> handleNoSuchUserException(
+            NoSuchUserException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
