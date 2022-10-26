127d30011d445e695a8d065d62b65e7c94c2d03d
sehun-kim <rogarithm@gmail.com>
2022-10-26 22:58:28 +0900

    Test: User에서 Optional<User> 변경 사항 테스트에 반영

--- src/test/java/com/flab/sooldama/domain/user/service/UserServiceTest.java
+++ src/test/java/com/flab/sooldama/domain/user/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.flab.sooldama.domain.user.service;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -13,13 +14,16 @@ import com.flab.sooldama.domain.user.dto.response.JoinUserResponse;
 import com.flab.sooldama.domain.user.exception.DuplicateEmailExistsException;
 import com.flab.sooldama.domain.user.exception.NoSuchUserException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 /*
  * @ExtendWith 어노테이션은 테스트에서 사용할 클래스를 명시합니다. @ExtendWith(MockitoExtension.class)를 사용함으로써
@@ -58,8 +62,18 @@ class UserServiceTest {
                 .build();
 
         doNothing().when(userMapper).insertUser(any(User.class));
-        when(userMapper.findUserByEmail(any(String.class))).thenReturn(user);
-        when(userMapper.findUserById(any(Long.class))).thenReturn(user);
+        when(userMapper.findUserById(any(Long.class))).thenReturn((Optional.of(user)));
+        //when(userMapper.findUserByEmail(any(String.class))).thenReturn(Optional.of(user));
+        when(userMapper.findUserByEmail(any(String.class))).thenAnswer(new Answer() {
+            private int count = 0;
+
+            public Object answer(InvocationOnMock invocation) {
+                if (++count == 1)
+                    return Optional.ofNullable(null);
+                else
+                    return Optional.of(user);
+            }
+        });
 
         // 실행
         JoinUserResponse response = userService.insertUser(request);
@@ -68,7 +82,7 @@ class UserServiceTest {
         // 행위 검증
         Assertions.assertThat(joinedUserResponse.getId()).isEqualTo(1L);
         verify(userMapper).insertUser(any(User.class));
-        verify(userMapper).findUserByEmail(any(String.class));
+        verify(userMapper, times(2)).findUserByEmail(any(String.class));
         verify(userMapper).findUserById(any(Long.TYPE));
     }
 
@@ -101,7 +115,6 @@ class UserServiceTest {
                 .isAdult(true)
                 .build();
 
-        doNothing().when(userMapper).insertUser(any(User.class));
         when(userMapper.findUserByEmail(any(String.class))).thenThrow(DuplicateEmailExistsException.class);
 
         // 실행
@@ -110,7 +123,6 @@ class UserServiceTest {
         });
 
         // 행위 검증
-        verify(userMapper).insertUser(any(User.class));
         verify(userMapper).findUserByEmail(any(String.class));
     }
 }
