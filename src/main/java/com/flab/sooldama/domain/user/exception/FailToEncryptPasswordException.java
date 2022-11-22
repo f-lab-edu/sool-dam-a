07dfc29d9ab662628e8acd84180ceca9dd6c29fd
sehun-kim <rogarithm@gmail.com>
2022-11-23 01:09:56 +0900

    Remove: FailToEncryptPasswordException 예외 삭제
     - 예외 처리 일관성 유지를 위해 이 예외를 삭제했다.

--- src/main/java/com/flab/sooldama/domain/user/exception/FailToEncryptPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.flab.sooldama.domain.user.exception;
-
-public class FailToEncryptPasswordException extends RuntimeException {
-
-	public FailToEncryptPasswordException(String message) {
-		super(message);
-	}
-}
