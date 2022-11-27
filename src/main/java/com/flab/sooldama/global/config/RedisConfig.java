6c311861279f37257035810ca3048c2adaa84484
sehun-kim <rogarithm@gmail.com>
2022-11-27 20:14:21 +0900

    Fix: redis를 쓰는 profile에서만 redis 설정을 가져오도록 명시

--- src/main/java/com/flab/sooldama/global/config/RedisConfig.java
+++ src/main/java/com/flab/sooldama/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.flab.sooldama.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@Profile("dev")
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = RedisConfig.EXPIRE_TIME)
 public class RedisConfig {
