ed610017447854006d35c0ce6e8c5683dc0b1b3b
sehun-kim <rogarithm@gmail.com>
2022-11-29 02:58:29 +0900

    Test: embedded redis 설정 클래스 추가

---
+++ src/test/java/com/flab/sooldama/global/config/EmbeddedRedisRepositoryConfig.java
@@ -0,0 +1,32 @@
+package com.flab.sooldama.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class EmbeddedRedisRepositoryConfig {
+
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory embeddedRedisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> embeddedRedisTemplate() {
+		RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(embeddedRedisConnectionFactory());
+		return redisTemplate;
+	}
+}
