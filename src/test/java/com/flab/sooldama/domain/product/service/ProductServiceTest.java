52fb69f507cd9792942090182dc438db9c3275a2
sehun-kim <rogarithm@gmail.com>
2022-12-06 23:26:18 +0900

    Test: 불필요한 import문 정리

--- src/test/java/com/flab/sooldama/domain/product/service/ProductServiceTest.java
+++ src/test/java/com/flab/sooldama/domain/product/service/ProductServiceTest.java
@@ -3,7 +3,6 @@ package com.flab.sooldama.domain.product.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
@@ -20,7 +19,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
