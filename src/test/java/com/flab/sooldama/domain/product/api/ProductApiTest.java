1c9d6c4fab8e38992446a3b5c2fdadd1cf01ea7c
sehun-kim <rogarithm@gmail.com>
2022-12-06 23:03:13 +0900

    Test: 테스트 데이터 생성 메소드 추가

--- src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
+++ src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
@@ -34,6 +34,46 @@ public class ProductApiTest {
 	@MockBean
 	ProductService productService;
 
+	private List<ProductResponse> products;
+
+	@BeforeEach
+	public void setUp() {
+		ProductResponse product1 = ProductResponse.builder()
+			.id(1L)
+			.productCategoryId(1L)
+			.name("백련 미스티 살균 막걸리")
+			.price(4500)
+			.imageUrl("https://www.sooldamhwa.com/images/common/mainLogo.png")
+			.description("연꽃이 들어간 살균 막걸리")
+			.abv(7.0)
+			.capacity(375)
+			.build();
+		ProductResponse product2 = ProductResponse.builder()
+			.id(2L)
+			.productCategoryId(1L)
+			.name("구름을 벗삼아")
+			.price(20000)
+			.imageUrl("https://www.sooldamhwa.com/images/common/mainLogo.png")
+			.description("구름처럼 부드럽고 달콤한 막걸리")
+			.abv(6.0)
+			.capacity(500)
+			.build();
+		ProductResponse product3 = ProductResponse.builder()
+			.id(3L)
+			.productCategoryId(2L)
+			.name("토박이 한산 소곡주")
+			.price(11000)
+			.imageUrl("https://www.sooldamhwa.com/images/common/mainLogo.png")
+			.description("한 번 마시면 멈출 수 없는 맛")
+			.abv(16.0)
+			.capacity(500)
+			.build();
+		this.products = new ArrayList<ProductResponse>();
+		products.add(product1);
+		products.add(product2);
+		products.add(product3);
+	}
+
 	@Test
 	@DisplayName("제품 조회 성공 테스트")
 	public void getProductsTest() throws Exception {
