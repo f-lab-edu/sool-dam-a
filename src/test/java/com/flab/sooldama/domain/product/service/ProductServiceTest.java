ff379c9e43f4afca6a162514d668cb874c48756b
sehun-kim <rogarithm@gmail.com>
2022-12-06 23:26:53 +0900

    Test: ProductService 테스트 mock 방식으로 변경

--- src/test/java/com/flab/sooldama/domain/product/service/ProductServiceTest.java
+++ src/test/java/com/flab/sooldama/domain/product/service/ProductServiceTest.java
@@ -17,12 +17,14 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
 	/*
