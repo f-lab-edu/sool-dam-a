623f038e00e7c38495544c4684bdba85cc511081
sehun-kim <rogarithm@gmail.com>
2022-12-06 23:07:59 +0900

    Test: 테스트할 클래스 표기 방식 UserApiTest와 일관성 맞추기

--- src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
+++ src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
@@ -22,7 +22,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ProductApi.class)
+@WebMvcTest(controllers = ProductApi.class)
 public class ProductApiTest {
 
 	@Autowired
