80b6c2f3ccbbad6f5c22f255afea4cef3388ef49
sehun-kim <rogarithm@gmail.com>
2022-12-06 22:58:08 +0900

    Test: mock 방식으로 테스트 변경에 따른 import문 추가

--- src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
+++ src/test/java/com/flab/sooldama/domain/product/api/ProductApiTest.java
@@ -1,11 +1,22 @@
 package com.flab.sooldama.domain.product.api;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.flab.sooldama.domain.product.dto.response.ProductResponse;
+import com.flab.sooldama.domain.product.exception.ProductNotFoundException;
 import com.flab.sooldama.domain.product.service.ProductService;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
